852a87ef9076344fd85a9107be480806a2cedbb0
park-jemin <jemin@live.unc.edu>
2022-03-17 19:57:10 -0400

    Create 70_Climbing_Stairs.c

---
+++ 70_Climbing_Stairs.c
@@ -0,0 +1,45 @@
+//
+
+
+int climbStairs(int n) {
+	int a = 1, b = 1;
+    while (n--)
+    	a = (b += a) - a;
+ 
+    return a;
+}
+
+int climbStairs2(int n){
+    // O(n) time, O(1) space
+    if (n <= 3)
+        return n;
+    
+    // track # ways in one step back and two steps back
+    int step1 = 2, step2 = 3;
+    for (int i = 4; i < n; i++) {
+        int nextStep = step1 + step2;
+        step1 = step2;
+        step2 = nextStep;
+    }
+    
+    return step1 + step2;
+}
+
+
+int climbStairs3(int n) {
+	// O(n) time, O(n) space
+    if (n <= 3)
+        return n;
+    
+    int* ways = malloc(n * sizeof(int));
+    ways[0] = 1;
+    ways[1] = 2;
+    for (int i = 2; i < n; i++) {
+        ways[i] = ways[i - 1] + ways[i - 2];
+    }
+    
+    int result = ways[n - 1];
+    free(ways);
+    return result;
+
+}
